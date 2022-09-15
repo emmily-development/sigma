f5345ec7af9d8f4210f8aa6af727a772a692eacc
emmilydev <Alextavriayt@gmail.com>
2022-09-15 16:06:32 -0300

    feat: create caffeine platform

--- platform/platform-caffeine/src/main/java/dev/emmily/sigma/platform/caffeine/LoadingCaffeineModelService.java
+++ platform/platform-caffeine/src/main/java/dev/emmily/sigma/platform/caffeine/LoadingCaffeineModelService.java
@@ -1,86 +1,70 @@
-package dev.emmily.sigma.api.service.builtin;
+package dev.emmily.sigma.platform.caffeine;
 
+import com.github.benmanes.caffeine.cache.*;
 import dev.emmily.sigma.api.Model;
 import dev.emmily.sigma.api.service.ModelService;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Predicate;
 
-/**
- * This is the default implementation of
- * {@link ModelService}, which, as its name
- * says, uses a {@link Map} to hold models.
- * It also serves as the first example on how
- * to create an own implementation of
- * {@link ModelService}, as it shows a basic
- * implementation of queries using
- * {@link Predicate predicates}.
- *
- * @param <T> The type of model held by this
- *           model service.
- * @author emmilydev
- */
-public class MapModelService<T extends Model>
+public class LoadingCaffeineModelService<T extends Model>
   implements ModelService<T> {
   private static final IllegalArgumentException INVALID_QUERY = new IllegalArgumentException(
-    "MapModelService only accepts queries of type Predicate<T extends Model>"
+    "LoadingCaffeineModelService only accepts queries of type String"
   );
-  private final Map<String, T> modelRegistry;
+  private final LoadingCache<String, T> cache;
 
-  public MapModelService(Map<String, T> modelRegistry) {
-    this.modelRegistry = modelRegistry;
+  public LoadingCaffeineModelService(LoadingCache<String, T> cache) {
+    this.cache = cache;
   }
 
-  public MapModelService() {
-    this(new ConcurrentHashMap<>());
+  public LoadingCaffeineModelService(
+    String spec,
+    CacheLoader<String, T> loader
+  ) {
+    this(Caffeine.from(spec).build(loader));
+  }
+
+  public LoadingCaffeineModelService(
+    CaffeineSpec spec,
+    CacheLoader<String, T> loader
+  ) {
+    this(Caffeine.from(spec).build(loader));
   }
 
   @Override
   public void create(T model) {
-    modelRegistry.put(model.getId(), model);
+    cache.put(model.getId(), model);
   }
 
   @Override
   public T find(String id) {
-    return modelRegistry.get(id);
+    return cache.get(id);
   }
 
   @Override
   public T findByQuery(Object query) {
-    if (!(query instanceof Predicate)) {
-      throw INVALID_QUERY;
+    if (query instanceof String) {
+      return find((String) query);
     }
 
-    @SuppressWarnings("unchecked")
-    Predicate<T> modelQuery = (Predicate<T>) query;
-
-    for (T model : modelRegistry.values()) {
-      if (modelQuery.test(model)) {
-        return model;
-      }
-    }
-    
-    return null;
+    throw INVALID_QUERY;
   }
 
   @Override
   public List<T> findMany(
-    List<String> ids, 
+    List<String> ids,
     int limit
   ) {
     List<T> models = new ArrayList<>();
 
-    for (T model : modelRegistry.values()) {
-      if (ids.contains(model.getId())) {
-        if (limit-- == 0) {
-          break;
-        }
-
-        models.add(model);
+    for (T model : cache.getAll(ids).values()) {
+      if (limit == 0) {
+        break;
       }
+
+      models.add(model);
     }
 
     return models;
@@ -100,7 +84,7 @@ public class MapModelService<T extends Model>
 
     List<T> models = new ArrayList<>();
 
-    for (T model : modelRegistry.values()) {
+    for (T model : cache.asMap().values()) {
       if (modelQuery.test(model)) {
         if (limit-- == 0) {
           break;
@@ -115,12 +99,12 @@ public class MapModelService<T extends Model>
 
   @Override
   public List<T> findAll() {
-    return new ArrayList<>(modelRegistry.values());
+    return new ArrayList<>(cache.asMap().values());
   }
 
   @Override
   public void delete(String id) {
-    modelRegistry.remove(id);
+    cache.invalidate(id);
   }
 
   @Override
@@ -132,18 +116,16 @@ public class MapModelService<T extends Model>
     @SuppressWarnings("unchecked")
     Predicate<T> modelQuery = (Predicate<T>) query;
 
-    for (T model : modelRegistry.values()) {
+    for (T model : cache.asMap().values()) {
       if (modelQuery.test(model)) {
-        modelRegistry.remove(model.getId());
+        cache.invalidate(model.getId());
       }
     }
   }
 
   @Override
   public void deleteMany(List<String> ids) {
-    for (String id : ids) {
-      modelRegistry.remove(id);
-    }
+    cache.invalidateAll(ids);
   }
 
   @Override
@@ -158,7 +140,7 @@ public class MapModelService<T extends Model>
     @SuppressWarnings("unchecked")
     Predicate<T> modelQuery = (Predicate<T>) query;
 
-    for (T model : modelRegistry.values()) {
+    for (T model : cache.asMap().values()) {
       if (modelQuery.test(model)) {
         if (limit-- == 0) {
           break;
@@ -169,3 +151,4 @@ public class MapModelService<T extends Model>
     }
   }
 }
+
